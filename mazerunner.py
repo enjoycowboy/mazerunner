159707cef02fb530f843b4d9b9ca92d6fc4c63d3
kstx-evolution <eveiga@msn.com>
2018-12-05 03:54:07 -0200

    agora ele tá caminhando dentro da porra toda, apesar de tá só recriando.
    TODO: ADICIONAR CRITÉRIO DE PARADA

--- mazerunner.py
+++ mazerunner.py
@@ -23,13 +23,13 @@ def createmaze(size):
 		vizinhos_livres=[] #lista de vizinhos disponiveis
 		for i in dirRange: #procurando nas 4 direcoes
 			nx = cx+dx[i] ; ny = cy+dy[i] #nx e ny sao as coord do no candidato
-			print("Looking at ("+str(nx)+","+str(ny)+")")
+			#print("Looking at ("+str(nx)+","+str(ny)+")")
 			if nx >=0 and nx<size and ny>=0 and ny<size:
 				if maze[ny][nx]==0: #se a celula estiver livre
 					vizinhos_ocupados = 0 #numero de vizinhos ocupados deve ser 1
 					for j in range(4):#procurando vizinhos nas 4 direcoes
 						vizinho_x = nx+dx[j]; vizinho_y = ny+dy[j] #ex sao os vizinhos do no candidato
-						print("Looking at ("+str(vizinho_x)+","+str(vizinho_y)+")")
+			#			print("Looking at ("+str(vizinho_x)+","+str(vizinho_y)+")")
 						if vizinho_x>=0 and vizinho_x<size and vizinho_y>=0 and vizinho_y<size:
 							if maze[vizinho_y][vizinho_x] ==1: vizinhos_ocupados+=1
 				if vizinhos_ocupados == 1: nlst.append(i)
@@ -39,7 +39,7 @@ def createmaze(size):
 		if len(nlst)>0:
 			ir = nlst[random.randint(0,len(nlst)-1)]
 			cx += dx[ir]; cy+=dy[ir]; maze[cy][cx]=1
-			print("Walking to ("+str(cx)+","+str(cy)+")")
+			#print("Walking to ("+str(cx)+","+str(cy)+")")
 			stack.append((cx,cy,ir))
 		else:stack.pop()
 
@@ -81,7 +81,6 @@ def solvemaze(maze, size):
 		(current_x, current_y,cd) = stack[-1]
 		for i in dirRange:
 			next_x = current_x + dx[i]; next_y = current_y + dy[i] 			# espaco em branco  -> maze[x][y] = 1
-			print("Looking at ("+str(next_x)+","+str(next_y)+")")
 			if next_x >= 0 and next_x < size and next_y >= 0 and next_y < size:
 				if maze[next_y][next_x] == 1:
 					ctr = 0
@@ -99,7 +98,7 @@ def solvemaze(maze, size):
 			stack.append((current_y,current_x,direction))
 		else: stack.pop()
 
-	return steps
+	return maze
 
 #solution = list of steps taken
 #def metrics(solution):
@@ -120,13 +119,13 @@ def main():
 		time.sleep(5)
 		steps = solvemaze(run,size)
 	except KeyboardInterrupt:
-		for ky in range(size):
+		exit(0)
+
+	for ky in range(size):
 			for kx in range(size):
 				pixels[kx, ky] = color[steps[size * ky / size][size * kx / size]]
 	
-		image.save("Solution_" + str(size) + "x" + str(size) + ".png", "PNG")
-		exit(0)
-
+	image.save("Solution_" + str(size) + "x" + str(size) + ".png", "PNG")
 if __name__ == '__main__':
 	main()
 
